6100336476b7269e177a916f34633b433f07bc50
Yuren Ju <yurenju@gmail.com>
2015-04-01 19:04:46 +0800

    Removed popup

--- app/scripts/popup.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var goBtn = document.getElementById('go');
-goBtn.addEventListener('click', function() {
-  var total = parseInt(document.getElementById('total').value);
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {total: total});
-  });
-});
